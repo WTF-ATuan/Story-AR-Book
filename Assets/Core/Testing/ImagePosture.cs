e3a8398de9ba0d8f5a81259e9e393ad8bac350cb
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2022-12-04 19:03:48 +0800

    added Testing ImagePosture

---
+++ Assets/Core/Testing/ImagePosture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+namespace Core.Testing{
+	[RequireComponent(typeof(ARTrackedImageManager))]
+	public class ImagePosture : MonoBehaviour{
+		private ARTrackedImageManager _imageTracker;
+
+		private void Start(){
+			_imageTracker = GetComponent<ARTrackedImageManager>();
+		}
+
+		private void OnEnable(){
+			_imageTracker.trackedImagesChanged += OnTrackedImagesChanged;
+		}
+
+		private void OnDisable(){
+			_imageTracker.trackedImagesChanged -= OnTrackedImagesChanged;
+		}
+
+		private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs obj){
+			foreach(var trackedImage in obj.added){
+				UpdateImage(trackedImage);
+			}
+
+			foreach(var trackedImage in obj.updated){
+				UpdateImage(trackedImage);
+			}
+
+			foreach(var trackedImage in obj.removed){
+				RemoveImage(trackedImage);
+			}
+		}
+
+		private void UpdateImage(ARTrackedImage trackedImage){ }
+		private void RemoveImage(ARTrackedImage trackedImage){ }
+	}
+}
