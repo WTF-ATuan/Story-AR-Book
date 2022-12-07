ace9b6b478e6f5db7c08ccb86766830b12f7cae4
ATuan <4A8K0069@stust.edu.tw>
2022-12-07 23:25:26 +0800

    added feature

--- Assets/Core/Testing/SpriteFacingCamera.cs
+++ Assets/Core/Testing/SpriteFacingCamera.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using UnityEngine;
+
+namespace Core.Testing{
+	public class SpriteFacingCamera : MonoBehaviour{
+		private void Update(){
+			if(Camera.main is not null)
+				transform.rotation = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0);
+		}
+	}
+}
