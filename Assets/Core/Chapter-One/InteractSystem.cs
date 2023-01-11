53d0676b36b9a7f1989253c848948605ed101d50
Atuan <jain60501@gmail.com.tw>
2023-01-11 21:11:28 +0800

    added Chapter one Model and play style

---
+++ Assets/Core/Chapter-One/InteractSystem.cs
@@ -0,0 +1,33 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.Testing{
+	public class InteractSystem : MonoBehaviour{
+		[SerializeField] private Button interactButton;
+
+		private void Start(){
+			interactButton.OnClickAsObservable().Subscribe(OnClick);
+		}
+
+		private void OnClick(Unit obj){
+			
+		}
+
+		private void OnTriggerEnter(Collider other){
+			if(!other.CompareTag("Interact")){
+				return;
+			}
+
+			interactButton.gameObject.SetActive(true);
+		}
+
+		private void OnTriggerExit(Collider other){
+			if(!other.CompareTag("Interact")){
+				return;
+			}
+
+			interactButton.gameObject.SetActive(false);
+		}
+	}
+}
