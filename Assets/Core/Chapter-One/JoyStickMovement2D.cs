4efdcadc20378d814fbdfce3d577992f7ba907ea
Atuan <jain60501@gmail.com.tw>
2023-01-17 19:12:07 +0800

    feat : added rigidbody movement to player

--- Assets/Core/Chapter-One/JoyStickMovement2D.cs
+++ Assets/Core/Chapter-One/JoyStickMovement2D.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 namespace Core.Testing{
 	public class JoyStickMovement2D : MonoBehaviour{
 		[Required] public VariableJoystick variableJoystick;
+		[SerializeField] private Transform playerRoot;
 		public float maxSpeed = 5;
 
-		public float acceleration = 20.0f;
-		public float deceleration = 20.0f;
+		private Rigidbody _rigidbody;
+
+		private void Start(){
+			_rigidbody = GetComponent<Rigidbody>();
+		}
 
 		private void Update(){
-			// Read Input values
-
 			var horizontal = variableJoystick.Horizontal;
 			var vertical = variableJoystick.Vertical;
 
-			// Create a movement direction vector (in world space)
-
 			var movementDirection = Vector3.zero;
-			movementDirection += transform.forward * vertical;
-			movementDirection += transform.right * horizontal;
+			movementDirection += playerRoot.forward * vertical;
+			movementDirection += playerRoot.right * horizontal;
 
-			// Make Sure it won't move faster diagonally
 
 			movementDirection = Vector3.ClampMagnitude(movementDirection, 1.0f);
-			
+
 			// Perform movement
 
 			var desiredVelocity = movementDirection * maxSpeed * Time.fixedDeltaTime;
-			transform.localPosition += desiredVelocity;
+			_rigidbody.MovePosition(_rigidbody.position + transform.TransformDirection(desiredVelocity));
 		}
 	}
 }
