74ab6260584bc94b1451e9372c0ddbe4e395824c
Atuan <jain60501@gmail.com.tw>
2023-01-18 17:34:00 +0800

    added interact system

---
+++ Assets/Core/Chapter-One/InteractRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using ModestTree;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Core{
+	public class InteractRepository : MonoBehaviour{
+		public List<Collider> interactObject;
+
+
+		public void RegisterCollision(string id, Action<Collider, bool> callback){
+			var foundCollider = interactObject.Find(x => x.name == id);
+			if(!foundCollider){
+				throw new Exception($"can,t find {id}");
+			}
+			foundCollider.OnTriggerEnterAsObservable().Subscribe(x => callback(x, true));
+			foundCollider.OnTriggerExitAsObservable().Subscribe(x => callback(x, false));
+		}
+
+		public void RegisterCollisionWithTag(string objectTag, bool enterOrExit, Action<Collider, string> callback){
+			var foundCollider = interactObject.FindAll(x => x.CompareTag(objectTag));
+			if(foundCollider.IsEmpty()){
+				throw new Exception($"can,t find {objectTag}");
+			}
+
+			foreach(var collider in foundCollider){
+				if(enterOrExit){
+					collider.OnTriggerEnterAsObservable().Subscribe(x => callback(x, collider.name));
+				}
+				else{
+					collider.OnTriggerExitAsObservable().Subscribe(x => callback(x, collider.name));
+				}
+			}
+		}
+	}
+}
--- Assets/Core/Chapter-One/InteractSystem.cs
+++ Assets/Core/Chapter-One/InteractSystem.cs
@@ -1,39 +1,47 @@
 ﻿using System;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Core.Testing{
 	public class InteractSystem : MonoBehaviour{
 		[SerializeField] private Button interactButton;
 		[SerializeField] private Text debugText;
-		
+
+		public InteractRepository interactRepository;
+
 		private void Start(){
 			interactButton.OnClickAsObservable().Subscribe(OnClick);
+			interactRepository = FindObjectOfType<InteractRepository>();
+			interactRepository.RegisterCollisionWithTag("Interact", true, TriggerEnter);
+			interactRepository.RegisterCollisionWithTag("Interact", false, TriggerExit);
+			interactRepository.RegisterCollision("Stairs", (obj, condition) => Teleport(true, condition));
+			// interactRepository.RegisterCollision("Stairs Target", (obj, condition) => Teleport(false, condition));
+			interactRepository.RegisterCollision("Exit", (obj, condition) => Exit());
 		}
 
-		private void Update(){
-			debugText.text = transform.position.ToString();
+		private void Teleport(bool toTarget, bool enterOrExit){
+			var target = interactRepository.interactObject.Find(x => x.name == "Stairs Target");
+			transform.position = target.transform.position;
+		}
+		private void Exit(){
+			debugText.text = "逃脫成功";
+			debugText.color = Color.red;
+			debugText.fontStyle = FontStyle.Bold;
+		}
+
+		private void TriggerEnter(Collider obj, string objID){
+			interactButton.gameObject.SetActive(true);
+			debugText.text = objID;
+		}
+
+		private void TriggerExit(Collider obj, string objID){
+			interactButton.gameObject.SetActive(false);
 		}
 
 		private void OnClick(Unit obj){
-			
-		}
-
-		private void OnTriggerEnter(Collider other){
-			if(!other.CompareTag("Interact")){
-				return;
-			}
-
-			interactButton.gameObject.SetActive(true);
-		}
-
-		private void OnTriggerExit(Collider other){
-			if(!other.CompareTag("Interact")){
-				return;
-			}
-
-			interactButton.gameObject.SetActive(false);
+			//需要一個可以設定 答案的資料庫，去比對答案
 		}
 	}
 }
