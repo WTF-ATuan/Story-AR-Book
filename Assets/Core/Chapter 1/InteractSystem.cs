4d5c97c92e10c928bde715c474774eeb1719aa44
Atuan <jain60501@gmail.com.tw>
2023-01-25 18:43:26 +0800

    Finish feat of interact Object can do all

--- Assets/Core/Chapter 1/InteractSystem.cs
+++ Assets/Core/Chapter 1/InteractSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Core.Chapter_1;
 using Sirenix.OdinInspector;
 using UniRx;
 using UnityEngine;
@@ -14,12 +15,12 @@ namespace Core.Testing{
 		[SerializeField] [ReadOnly] private List<string> interactNames = new List<string>();
 
 		public InteractRepository interactRepository;
-		[SerializeField] private GameObject news;
-		[Inject] private readonly InteractDataSet _interactDataSet;
+		[SerializeField] private InteractUI interactUI;
 
 
 		private void Start(){
 			interactRepository = FindObjectOfType<InteractRepository>();
+			interactUI = FindObjectOfType<InteractUI>(true);
 			interactButton.OnClickAsObservable().Subscribe(x => OnClick());
 			interactRepository.RegisterCollisionWithTag("Interact", true, TriggerEnter);
 			interactRepository.RegisterCollisionWithTag("Interact", false, TriggerExit);
@@ -46,27 +47,12 @@ namespace Core.Testing{
 
 		private void OnClick(){
 			ShowInteractUI();
+			interactButton.gameObject.SetActive(false);
 		}
 
 		private void ShowInteractUI(){
 			var currentInteract = interactNames.First();
-			if(!currentInteract.Equals("News")) return;
-			// 報紙的純UI
-			news.SetActive(true);
-			var button = news.GetComponentInChildren<Button>();
-			button.OnClickAsObservable().Subscribe(x => OnInteractButtonClick());
-			//關閉按鈕UI
-			interactButton.gameObject.SetActive(false);
-		}
-
-		private void OnInteractButtonClick(){
-			if(_interactDataSet.CheckCorrect(interactNames.First())){
-				Debug.Log($"找到錯誤音效! 給予一個 '通關條件' ");
-			}
-			else{
-				Debug.Log($"這是正確的音效");
-			}
-			news.SetActive(false);
+			interactUI.ChangeUIData(currentInteract);
 		}
 	}
 }
--- Assets/Core/Chapter 1/InteractUI.cs
+++ Assets/Core/Chapter 1/InteractUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Sirenix.OdinInspector;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -24,8 +25,9 @@ namespace Core.Chapter_1{
 
 
 		private AnimatorOverrideController _overrideController;
-		
+
 		[Inject] private readonly InteractDataSet _interactDataSet;
+		private string _currentObjID;
 
 		private void Start(){
 			image = imageComponent.sprite;
@@ -33,6 +35,7 @@ namespace Core.Chapter_1{
 			buttonTransform = buttonComponent.GetComponent<RectTransform>();
 			audioClip = audioComponent.clip;
 			_overrideController = new AnimatorOverrideController(animationComponent.runtimeAnimatorController);
+			buttonComponent.OnClickAsObservable().Subscribe(x => OnInteractButtonClick());
 		}
 
 		[Button]
@@ -42,8 +45,16 @@ namespace Core.Chapter_1{
 			animationClip = interactData.animationClip;
 			buttonTransform = interactData.buttonTransform;
 			audioClip = interactData.audioClip;
-			
+			_currentObjID = objID;
 			SetDataToComponent();
+			SetComponentActive(true);
+		}
+
+		private void SetComponentActive(bool active){
+			imageComponent.gameObject.SetActive(active);
+			animationComponent.gameObject.SetActive(active);
+			buttonComponent.gameObject.SetActive(active);
+			audioComponent.gameObject.SetActive(active);
 		}
 
 		private void SetDataToComponent(){
@@ -54,6 +65,17 @@ namespace Core.Chapter_1{
 			animationComponent.runtimeAnimatorController = _overrideController;
 			//Button .....
 			audioComponent.clip = audioClip;
+		}
+
+		private void OnInteractButtonClick(){
+			if(_interactDataSet.CheckCorrect(_currentObjID)){
+				Debug.Log($"找到錯誤音效! 給予一個 '通關條件' ");
+			}
+			else{
+				Debug.Log($"這是正確的音效");
+			}
+
+			SetComponentActive(false);
 		}
 	}
 }
