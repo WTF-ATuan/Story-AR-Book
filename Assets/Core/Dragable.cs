29604cdf5d9cd2a1fc3837b864d11e86fc7645c0
Atuan <jain60501@gmail.com.tw>
2023-02-02 13:49:59 +0800

    feat Dragable object

---
+++ Assets/Core/Dragable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core{
+	public class Dragable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDragHandler{
+		public Transform movingObject;
+		private bool _isPointerOver = false;
+
+		public void OnPointerEnter(PointerEventData eventData){
+			_isPointerOver = true;
+		}
+
+		public void OnPointerExit(PointerEventData eventData){
+			_isPointerOver = false;
+		}
+
+		public void OnDrag(PointerEventData eventData){
+			if(!_isPointerOver){
+				return;
+			}
+
+			if(movingObject){
+				movingObject.position = Input.mousePosition;
+			}
+			else{
+				transform.position = Input.mousePosition;
+			}
+		}
+	}
+}
