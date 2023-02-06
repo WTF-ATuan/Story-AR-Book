2379a701e5cd20529d26bd1ca2211c8e8bde29d6
Atuan <jain60501@gmail.com.tw>
2023-02-06 21:46:24 +0800

    added new puzzle prefab

---
+++ Assets/Core/Chapter 1/Clickable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Core.Chapter_1{
+	public class Clickable : MonoBehaviour, IPointerClickHandler{
+		public UnityEvent onClick;
+		public UnityEvent<bool> switchClick;
+		private bool _flagSwitch;
+		public void OnPointerClick(PointerEventData eventData){
+			onClick?.Invoke();
+			switchClick?.Invoke(_flagSwitch);
+			_flagSwitch = !_flagSwitch;
+		}
+	}
+}
