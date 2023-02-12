c550058811b01e5e632db1d7ac6bd12fabda4c68
Atuan <jain60501@gmail.com.tw>
2023-02-12 18:20:04 +0800

    update UI and Canvas

--- Assets/Core/Chapter 1/InteractSystem.cs
+++ Assets/Core/Chapter 1/InteractSystem.cs
@@ -4,28 +4,27 @@ using System.Linq;
 using Core.Chapter_1;
 using Sirenix.OdinInspector;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
 namespace Core.Testing{
 	public class InteractSystem : MonoBehaviour{
-		[SerializeField] private Button interactButton;
-
+		[SerializeField] private Image interactButton;
 		[Inject] public InteractRepository interactRepository;
 		[Inject] private InteractUI _interactUI;
 		[Inject] private readonly InteractDataSet _interactDataSet;
-
 		private Teleport _teleport;
 		private InteractTag _interactTag;
-		
+
 		[TitleGroup("Debug")] [SerializeField] [ReadOnly]
 		private List<string> interactNames = new List<string>();
 
 		[SerializeField] private Text debugText;
 
 		private void Start(){
-			interactButton.OnClickAsObservable().Subscribe(x => Interact());
+			interactButton.OnPointerClickAsObservable().Subscribe(x => Interact());
 			interactRepository.RegisterAll(CompareData);
 			interactRepository.RegisterAll(UpdateInteract);
 			_teleport = new Teleport(transform, interactRepository);
@@ -59,10 +58,8 @@ namespace Core.Testing{
 				default:
 					throw new ArgumentOutOfRangeException();
 			}
-
-
-			interactButton.gameObject.SetActive(false);
 		}
+
 		private void UpdateInteract(string objID, bool enterOrExit){
 			if(enterOrExit){
 				interactNames.Insert(0, objID);
@@ -72,10 +69,15 @@ namespace Core.Testing{
 					interactNames.Remove(objID);
 				}
 			}
-			interactButton.gameObject.SetActive(interactNames.Count > 0);
-			if(interactNames.Count > 0)
-				debugText.text = interactNames.First();
-		}
 
+			var activeObj = interactButton.transform.GetChild(0).gameObject;
+			if(interactNames.Count > 0){
+				debugText.text = interactNames.First();
+				activeObj.SetActive(true);
+			}
+			else{
+				activeObj.SetActive(false);
+			}
+		}
 	}
 }
