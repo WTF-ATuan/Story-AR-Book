7d80d367a8d9ac9e61550ee29119f72c711d5f5a
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-02-13 18:10:07 +0800

    refactor InteractLevel.cs remove all field

--- Assets/Core/Chapter 1/InteractLevel.cs
+++ Assets/Core/Chapter 1/InteractLevel.cs
@@ -1,17 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using Sirenix.OdinInspector;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
-using UnityEngine.UI;
 using Zenject;
 
 namespace Core.Chapter_1{
 	public class InteractLevel : MonoBehaviour{
-		[TitleGroup("Components")]
-		[SerializeField] [Required] private Animator animationComponent;
-		[SerializeField] [Required] private AudioSource audioComponent;
 		[Inject] private readonly PlayerData _playerData;
 		[Inject] private readonly UIPresenter _presenter;
 		private readonly List<GameObject> _levelRootList = new List<GameObject>();
@@ -22,21 +16,13 @@ namespace Core.Chapter_1{
 
 		public void Interact(InteractAnimationData data){
 			if(data.correctAnswer){
-				SetAnimation(data.animationClip);
-				audioComponent.clip = data.audioClip;
-				SetComponentActive(true);
+				_presenter.SetAnimation(data.animationClip);
+				_presenter.SetAudio(data.audioClip);
+				_presenter.SetLevelActive(true);
 			}
 			else{
 				CreateOrOpenLevel(data.interactiveRoot);
 			}
-		}
-
-		private void SetAnimation(AnimationClip clip){
-			var overrideController = new AnimatorOverrideController(animationComponent.runtimeAnimatorController);
-			var animations = overrideController.animationClips.Select(animationClip =>
-					new KeyValuePair<AnimationClip, AnimationClip>(animationClip, clip)).ToList();
-			overrideController.ApplyOverrides(animations);
-			animationComponent.runtimeAnimatorController = overrideController;
 		}
 
 		private void CreateOrOpenLevel(InteractiveRoot root){
@@ -57,15 +43,10 @@ namespace Core.Chapter_1{
 			ExitFocusMode();
 		}
 
-		private void SetComponentActive(bool active){
-			animationComponent.gameObject.SetActive(active);
-			audioComponent.gameObject.SetActive(active);
-		}
-
 		private void ExitFocusMode(){
 			_levelRootList.ForEach(x => x.SetActive(false));
+			_presenter.SetLevelActive(false);
 			_presenter.SwitchMode(UIMode.PlayMode);
-			SetComponentActive(false);
 		}
 	}
 }
--- Assets/Core/Chapter 1/UIPresenter.cs
+++ Assets/Core/Chapter 1/UIPresenter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -7,13 +8,15 @@ using UnityEngine.UI;
 
 namespace Core.Chapter_1{
 	public class UIPresenter : MonoBehaviour{
-		[TitleGroup("Play Mode")] public Image findIcon;
-		[TitleGroup("Play Mode")] public Image nameTagView;
-		[TitleGroup("Play Mode")] public Text nameTag;
-		[TitleGroup("Play Mode")] public Image setting;
-		[TitleGroup("Play Mode")] public List<Image> completePartList;
-		[TitleGroup("Interact Focus")] public Image interactBackGround;
-		[TitleGroup("Interact Focus")] public Image exitButton;
+		[FoldoutGroup("Play Mode")] public Image findIcon;
+		[FoldoutGroup("Play Mode")] public Image nameTagView;
+		[FoldoutGroup("Play Mode")] public Text nameTag;
+		[FoldoutGroup("Play Mode")] public Image setting;
+		[FoldoutGroup("Interact Focus")] public Image interactBackGround;
+		[FoldoutGroup("Interact Focus")] public Image exitButton;
+		[FoldoutGroup("Interact Focus")] public Animator levelAnimator;
+		[FoldoutGroup("Interact Focus")] public AudioSource levelAudio;
+		[FoldoutGroup("Goal")] public List<Image> completePartList;
 
 		[Button]
 		public void SwitchMode(UIMode mode){
@@ -42,6 +45,22 @@ namespace Core.Chapter_1{
 		public void SetFindState(bool active){
 			var activeObj = findIcon.transform.GetChild(0);
 			activeObj.gameObject.SetActive(active);
+		}
+		public void SetAnimation(AnimationClip clip){
+			var overrideController = new AnimatorOverrideController(levelAnimator.runtimeAnimatorController);
+			var animations = overrideController.animationClips.Select(animationClip =>
+					new KeyValuePair<AnimationClip, AnimationClip>(animationClip, clip)).ToList();
+			overrideController.ApplyOverrides(animations);
+			levelAnimator.runtimeAnimatorController = overrideController;
+		}
+
+		public void SetAudio(AudioClip clip){
+			levelAudio.clip = clip;
+		}
+
+		public void SetLevelActive(bool active){
+			levelAnimator.gameObject.SetActive(active);
+			levelAudio.gameObject.SetActive(active);
 		}
 	}
 
