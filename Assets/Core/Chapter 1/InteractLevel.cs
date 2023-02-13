6e622c666457adc1a6de579968ff9085224d053b
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-02-13 17:56:21 +0800

    fixed animation with Interat Level

--- Assets/Core/Chapter 1/InteractLevel.cs
+++ Assets/Core/Chapter 1/InteractLevel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using UniRx;
 using UniRx.Triggers;
@@ -8,28 +9,34 @@ using Zenject;
 
 namespace Core.Chapter_1{
 	public class InteractLevel : MonoBehaviour{
-		[TitleGroup("Components")] [SerializeField] [Required]
-		private Image imageComponent;
-
-		[SerializeField] [Required] private Animation animationComponent;
+		[TitleGroup("Components")]
+		[SerializeField] [Required] private Animator animationComponent;
 		[SerializeField] [Required] private AudioSource audioComponent;
 		[Inject] private readonly PlayerData _playerData;
 		[Inject] private readonly UIPresenter _presenter;
 		private readonly List<GameObject> _levelRootList = new List<GameObject>();
+
 		private void Start(){
 			_presenter.exitButton.OnPointerClickAsObservable().Subscribe(x => ExitFocusMode());
 		}
 
 		public void Interact(InteractAnimationData data){
 			if(data.correctAnswer){
-				imageComponent.sprite = data.image;
-				animationComponent.clip = data.animationClip;
+				SetAnimation(data.animationClip);
 				audioComponent.clip = data.audioClip;
 				SetComponentActive(true);
 			}
 			else{
 				CreateOrOpenLevel(data.interactiveRoot);
 			}
+		}
+
+		private void SetAnimation(AnimationClip clip){
+			var overrideController = new AnimatorOverrideController(animationComponent.runtimeAnimatorController);
+			var animations = overrideController.animationClips.Select(animationClip =>
+					new KeyValuePair<AnimationClip, AnimationClip>(animationClip, clip)).ToList();
+			overrideController.ApplyOverrides(animations);
+			animationComponent.runtimeAnimatorController = overrideController;
 		}
 
 		private void CreateOrOpenLevel(InteractiveRoot root){
@@ -51,7 +58,6 @@ namespace Core.Chapter_1{
 		}
 
 		private void SetComponentActive(bool active){
-			imageComponent.gameObject.SetActive(active);
 			animationComponent.gameObject.SetActive(active);
 			audioComponent.gameObject.SetActive(active);
 		}
