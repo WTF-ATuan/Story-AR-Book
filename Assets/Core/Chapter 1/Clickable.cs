3ca9e2c0226ccd1013a51d75a553fb87517bcfd6
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-02-15 12:32:01 +0800

    feat Lamp interact and added slidable to handle slide action

--- Assets/Core/Chapter 1/Clickable.cs
+++ Assets/Core/Chapter 1/Clickable.cs
@@ -1,17 +1,29 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace Core.Chapter_1{
-	public class Clickable : MonoBehaviour, IPointerClickHandler{
+	public class Clickable : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler{
+		public bool showMore = true;
 		public UnityEvent<bool> switchClick;
 		public UnityEvent<bool> switchNegativeClick;
+		[ShowIf("$showMore")] public UnityEvent pointDown;
+		[ShowIf("$showMore")] public UnityEvent pointUp;
 		private bool _flagSwitch;
 
 		public void OnPointerClick(PointerEventData eventData){
 			switchClick?.Invoke(_flagSwitch);
 			switchNegativeClick?.Invoke(!_flagSwitch);
 			_flagSwitch = !_flagSwitch;
+		}
+
+		public void OnPointerDown(PointerEventData eventData){
+			pointDown?.Invoke();
+		}
+
+		public void OnPointerUp(PointerEventData eventData){
+			pointUp?.Invoke();
 		}
 	}
 }
---
+++ Assets/Core/Chapter 1/Slidable.cs
@@ -0,0 +1,46 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Core.Chapter_1{
+	public class Slidable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler{
+		private Vector2 _pointerDownPos;
+		private Vector2 _pointerUpPos;
+		public UnityEvent slideLeft;
+		public UnityEvent slideRight;
+
+		[HideLabel] [HorizontalGroup] public int invokeCount = 2;
+		[HorizontalGroup] [HideLabel] public UnityEvent afterInvokeCount;
+
+		private int _invokeTimes;
+
+		public void OnPointerDown(PointerEventData eventData){
+			_pointerDownPos = eventData.position;
+		}
+
+		public void OnPointerUp(PointerEventData eventData){
+			_pointerUpPos = eventData.position;
+			CalculateSlide();
+		}
+
+		private void CalculateSlide(){
+			var pointerOffset = _pointerUpPos - _pointerDownPos;
+			if(pointerOffset.magnitude < 10){
+				return;
+			}
+
+			if(pointerOffset.x > 0){
+				slideRight?.Invoke();
+			}
+			else{
+				slideLeft?.Invoke();
+			}
+
+			_invokeTimes++;
+			if(_invokeTimes == invokeCount){
+				afterInvokeCount.Invoke();
+			}
+		}
+	}
+}
