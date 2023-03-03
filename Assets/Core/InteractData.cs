d80abe2d4bb951710efb2b077b3df843168e32db
Atuan <jain60501@gmail.com.tw>
2023-03-03 16:07:38 +0800

    added story guide data strust

--- Assets/Core/InteractData.cs
+++ Assets/Core/InteractData.cs
@@ -12,20 +12,35 @@ namespace Core{
 		public InteractTag tag = InteractTag.None;
 
 		[ReadOnly] [HideInInspector] public int instanceID;
-		
+
 		[ShowIf("tag", InteractTag.Teleport)] [BoxGroup("Teleport"), HideLabel]
 		public TeleportData teleportData;
 
 		[ShowIf("tag", InteractTag.InteractAnimation)] [BoxGroup("InteractAnimation"), HideLabel]
 		public InteractAnimationData interactAnimationData;
 
+		[ShowIf("tag", InteractTag.InteractAnimation)]
+		public bool storyGuide;
+
+		[ShowIf("IsShowStoryGuide")] [BoxGroup("StoryGuide"), HideLabel]
+		public StoryGuideData storyGuideData;
 
 		private List<ValueDropdownItem> GetDataTag(){
 			return new List<ValueDropdownItem>{
 				new ValueDropdownItem("None", InteractTag.None),
 				new ValueDropdownItem("InteractAnimation", InteractTag.InteractAnimation),
 				new ValueDropdownItem("Teleport ", InteractTag.Teleport),
+				new ValueDropdownItem("StoryGuide", InteractTag.StoryGuide)
 			};
+		}
+
+		private bool IsShowStoryGuide(){
+			if(tag == InteractTag.InteractAnimation){
+				return storyGuide;
+			}
+
+			return tag == InteractTag.StoryGuide;
+
 		}
 	}
 
@@ -33,6 +48,6 @@ namespace Core{
 		None,
 		Teleport,
 		InteractAnimation,
-		Condition
+		StoryGuide,
 	}
 }
---
+++ Assets/Core/StoryGuideData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Core{
+	[Serializable]
+	public class StoryGuideData{
+		public bool interact;
+		public bool showOnes;
+
+		[ListDrawerSettings(NumberOfItemsPerPage = 5, ElementColor = "GetColor")]
+		public List<StoryData> storyContext;
+
+
+		private Color GetColor(){
+			return Color.gray;
+		}
+
+		[Serializable]
+		public class StoryData{
+			[ValueDropdown("GetUpDown")] public bool upOrDown;
+			[TextArea] public string storyText;
+
+			private List<ValueDropdownItem> GetUpDown(){
+				return new List<ValueDropdownItem>{
+					new ValueDropdownItem("Up", true),
+					new ValueDropdownItem("Down", false),
+				};
+			}
+		}
+	}
+}
