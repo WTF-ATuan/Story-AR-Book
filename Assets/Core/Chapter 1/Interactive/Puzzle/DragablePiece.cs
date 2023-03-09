8ff51a37106bc2e99e4b965e07d4e3c6ec4022b1
Atuan <jain60501@gmail.com.tw>
2023-03-09 18:10:43 +0800

    added puzzle (WIP)

---
+++ Assets/Core/Chapter 1/Interactive/Puzzle/DragablePiece.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Core.Chapter_1.Interactive.Puzzle{
+	public class DragablePiece : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler{
+		private Vector3 _basePos;
+		private Vector3 _vec;
+		private Vector3 _startPos;
+		private CanvasGroup _canvasGroup;
+
+		private void Awake(){
+			var localPosition = transform.localPosition;
+			_startPos = localPosition;
+			_basePos = localPosition;
+			_canvasGroup = GetComponent<CanvasGroup>();
+		}
+
+		private const float Smooth = 0.1f;
+
+		private void Update(){
+			if(_draging) return;
+			transform.localPosition = (transform.localPosition - _basePos).magnitude < 0.1f
+					? _basePos
+					: Vector3.SmoothDamp(transform.localPosition, _basePos, ref _vec, Smooth);
+		}
+
+		private bool _draging;
+		private Vector2 _deviation;
+
+		public void OnDrag(PointerEventData eventData){
+			transform.localPosition = eventData.position + _deviation;
+		}
+
+		public void OnBeginDrag(PointerEventData eventData){
+			_draging = true;
+			_deviation = (Vector2)transform.localPosition - eventData.position;
+			_canvasGroup.blocksRaycasts = false;
+			_canvasGroup.alpha = 0.7f;
+		}
+
+		public void OnEndDrag(PointerEventData eventData){
+			_draging = false;
+			_canvasGroup.blocksRaycasts = true;
+			_canvasGroup.alpha = 1f;
+		}
+
+		public void ResetPosition(Transform root){
+			transform.SetParent(root);
+			transform.localPosition = Vector3.zero;
+			_basePos = transform.localPosition;
+		}
+	}
+}
---
+++ Assets/Core/Chapter 1/Interactive/Puzzle/PuzzlePlaceHolder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Core.Chapter_1.Interactive.Puzzle{
+	public class PuzzlePlaceHolder : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+		public GameObject pieceRoot;
+		private List<Image> _piece;
+		private bool _isMouseEnter;
+
+		private void Start(){
+			_piece = pieceRoot.GetComponentsInChildren<Image>(true).ToList();
+			_piece.ForEach(x => x.OnEndDragAsObservable().Subscribe(AssignToPlace));
+		}
+
+		private void AssignToPlace(PointerEventData data){
+			if(!_isMouseEnter || transform.childCount > 0) return;
+			var dragObj = data.pointerDrag.gameObject;
+			dragObj.GetComponent<DragablePiece>().ResetPosition(transform);
+		}
+
+		public void OnPointerEnter(PointerEventData eventData){
+			_isMouseEnter = true;
+		}
+
+		public void OnPointerExit(PointerEventData eventData){
+			_isMouseEnter = false;
+		}
+	}
+}
