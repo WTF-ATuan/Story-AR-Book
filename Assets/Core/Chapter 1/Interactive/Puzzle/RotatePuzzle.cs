278af6e7ebdc276bfd40ad4fceb963444a99f17d
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-03-10 12:50:02 +0800

    feat : rotate puzzle using dotween

---
+++ Assets/Core/Chapter 1/Interactive/Puzzle/RotatePuzzle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DG.Tweening;
+using UniRx;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
+
+namespace Core.Chapter_1.Interactive.Puzzle{
+	public class RotatePuzzle : MonoBehaviour, IPointerClickHandler{
+		[SerializeField] private bool randomRotate = true;
+		public IObservable<Unit> PuzzleAction;
+
+		private bool _freezeFlag;
+
+		private void Start(){
+			if(!randomRotate) return;
+			var range = Random.Range(1, 4);
+			var localEulerAngles = transform.localEulerAngles;
+			transform.DOLocalRotate(new Vector3(localEulerAngles.x, localEulerAngles.y, range * 90), 0.5f);
+		}
+
+		public void OnPointerClick(PointerEventData eventData){
+			if(_freezeFlag) return;
+			_freezeFlag = true;
+			transform.DOLocalRotate(new Vector3(0, 0, transform.localEulerAngles.z + 90), 0.5f)
+					.OnComplete(() => _freezeFlag = false);
+		}
+
+		public bool Complete(){
+			return transform.localEulerAngles.z == 0;
+		}
+	}
+}
