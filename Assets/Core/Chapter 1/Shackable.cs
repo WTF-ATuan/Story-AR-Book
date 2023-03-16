ab83d9ce7b5eed17f970a42c223290957b3bd46c
Atuan <jain60501@gmail.com.tw>
2023-03-16 14:45:24 +0800

    added new lamp shakeable

---
+++ Assets/Core/Chapter 1/Shackable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Core.Chapter_1{
+	public class Shackable : MonoBehaviour{
+		
+		[HideLabel] [HorizontalGroup] public int invokeCount = 2;
+		[HorizontalGroup] [HideLabel] public UnityEvent afterInvokeCount;
+
+		private int _invokeTimer = 0;
+		private float _countTimer;
+		private void Update(){
+			if(Input.acceleration.magnitude > 2){
+				if(_countTimer <= 0.25f){
+					return;
+				}
+
+				_invokeTimer++;
+				if(_invokeTimer >= invokeCount){
+					afterInvokeCount?.Invoke();
+				}
+			}
+
+			_countTimer += Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Core/Chapter 1/SimpleRotator.cs
@@ -0,0 +1,11 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Core.Chapter_1{
+	public class SimpleRotator : MonoBehaviour{
+		
+		public void RotateOffsetZ(float zOffset){
+			transform.DOLocalRotate(new Vector3(0, 0, zOffset), 0.25f);
+		}
+	}
+}
