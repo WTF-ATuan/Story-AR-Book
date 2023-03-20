16493eded047131c85832cb026d41ba37e048ae3
Atuan <jain60501@gmail.com.tw>
2023-03-20 16:24:10 +0800

    added start menu video

---
+++ Assets/Core/Chapter 1/VideoEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+namespace Core.Chapter_1{
+	[RequireComponent(typeof(VideoPlayer))]
+	public class VideoEventHandler : MonoBehaviour{
+		[SerializeField] private UnityEvent finishVideo;
+		private VideoPlayer _videoPlayer;
+
+		private void Start(){
+			_videoPlayer = GetComponent<VideoPlayer>();
+			_videoPlayer.loopPointReached += OnVideoFinish;
+		}
+
+		private void OnVideoFinish(VideoPlayer source){
+			finishVideo?.Invoke();
+		}
+	}
+}
