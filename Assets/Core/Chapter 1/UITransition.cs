e8999265443ca6a683af5dfdca41ac1ef32f6536
Atuan <jain60501@gmail.com.tw>
2023-03-24 14:32:42 +0800

    fixed bug and added ui Transition

---
+++ Assets/Core/Chapter 1/UITransition.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.Chapter_1{
+	public class UITransition : MonoBehaviour{
+		public void ImageTransition(Image image){
+			image.DOFade(0, 0.5f).OnComplete(() => image.gameObject.SetActive(false));
+		}
+
+		public void TextTransition(Text text){
+			text.DOFade(0, 0.5f).OnComplete(() => text.transform.parent.gameObject.SetActive(false));
+		}
+	}
+}
