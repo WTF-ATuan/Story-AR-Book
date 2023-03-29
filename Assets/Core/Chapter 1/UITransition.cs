0db65b8ffdb2032d2e9674e60f81874d5534921b
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-03-29 16:06:54 +0800

    using dotween to transition UI offset

--- Assets/Core/Chapter 1/UITransition.cs
+++ Assets/Core/Chapter 1/UITransition.cs
@@ -11,5 +11,15 @@ namespace Core.Chapter_1{
 		public void TextTransition(Text text){
 			text.DOFade(0, 0.5f).OnComplete(() => text.transform.parent.gameObject.SetActive(false));
 		}
+
+		//move image to offset
+		public void ImageMove(Image image, Vector2 offset){
+			image.transform.DOLocalMove(offset, 0.5f);
+		}
+
+		//move text to offset
+		public void TextMove(Text text, Vector2 offset){
+			text.transform.parent.DOLocalMove(offset, 0.5f);
+		}
 	}
 }
