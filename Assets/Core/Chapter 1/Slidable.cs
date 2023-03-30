513962b2d38b5ac0f25b5f93228cb403368316d2
Atuan <jain60501@gmail.com.tw>
2023-03-30 16:28:38 +0800

    interact with lamp using drag to control lamp rotation

--- Assets/Core/Chapter 1/Slidable.cs
+++ Assets/Core/Chapter 1/Slidable.cs
@@ -1,45 +1,46 @@
-﻿using Sirenix.OdinInspector;
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace Core.Chapter_1{
-	public class Slidable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler{
+	public class Slidable : MonoBehaviour, IPointerDownHandler, IDragHandler{
 		private Vector2 _pointerDownPos;
 		private Vector2 _pointerUpPos;
-		public UnityEvent slideLeft;
-		public UnityEvent slideRight;
 
 		[HideLabel] [HorizontalGroup] public int invokeCount = 2;
 		[HorizontalGroup] [HideLabel] public UnityEvent afterInvokeCount;
 
-		private int _invokeTimes;
+		private float _invokeTimes;
 
 		public void OnPointerDown(PointerEventData eventData){
 			_pointerDownPos = eventData.position;
 		}
 
-		public void OnPointerUp(PointerEventData eventData){
-			_pointerUpPos = eventData.position;
-			CalculateSlide();
-		}
-
-		private void CalculateSlide(){
-			var pointerOffset = _pointerUpPos - _pointerDownPos;
+		public void OnDrag(PointerEventData eventData){
+			var pointerOffset = eventData.position - _pointerDownPos;
 			if(pointerOffset.magnitude < 10){
 				return;
 			}
 
-			if(pointerOffset.x > 0){
-				slideRight?.Invoke();
+			if(pointerOffset.x < 0){
+				if(transform.eulerAngles.z < 0){
+					_invokeTimes += Time.deltaTime;
+				}
+
+				transform.DOLocalRotate(new Vector3(0, 0, 20), 0.25f);
 			}
 			else{
-				slideLeft?.Invoke();
+				if(transform.eulerAngles.z > 0){
+					_invokeTimes += Time.deltaTime;
+				}
+
+				transform.DOLocalRotate(new Vector3(0, 0, -20), 0.25f);
 			}
 
-			_invokeTimes++;
-			if(_invokeTimes == invokeCount){
-				afterInvokeCount.Invoke();
+			if(_invokeTimes > invokeCount){
+				afterInvokeCount?.Invoke();
 			}
 		}
 	}
