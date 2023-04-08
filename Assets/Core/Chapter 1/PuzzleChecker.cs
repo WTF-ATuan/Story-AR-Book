8d79a9849b21bb7d3a7cf5e5e1204f95d5a1338c
Atuan <jain60501@gmail.com.tw>
2023-04-08 17:43:58 +0800

    added when puzzle pass will active last puzzle part

---
+++ Assets/Core/Chapter 1/PuzzleChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Chapter_1{
+	public class PuzzleChecker : MonoBehaviour{
+		[Inject] private readonly PlayerData _playerData;
+		[SerializeField] private GameObject[] activePart;
+
+		private void OnEnable(){
+			OnPuzzlePass(_playerData.GetSuccessID());
+		}
+
+		private void OnPuzzlePass(List<string> passIDList){
+			foreach(var part in from passID in passIDList
+					from part in activePart
+					where part.name == passID
+					select part){
+				part.SetActive(true);
+			}
+		}
+	}
+}
