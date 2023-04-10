59f06e266eab72391492f6d87e2c0bbb2d799417
Atuan <jain60501@gmail.com.tw>
2023-04-10 16:48:42 +0800

    added Transition animation

---
+++ Assets/Core/Chapter 1/AnimationHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Core.Chapter_1{
+	public class AnimationHandler : MonoBehaviour{
+		public UnityEvent onEvent;
+
+		public void AnimationEventTrigger(){
+			onEvent?.Invoke();
+		}
+	}
+}
